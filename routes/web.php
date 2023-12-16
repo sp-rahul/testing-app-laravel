c32bc85348bc83eff0e90eb2b07fe76d309c06cf
sp-rahul <jksruhulamin123@gmail.com>
2023-12-16 23:28:07 +0600

    login registration add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{productId}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::put('/product/{productId}/update', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{productId}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+
+
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/{productId}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::put('/product/{productId}/update', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{productId}/destroy', [ProductController::class, 'destroy'])->name('product.destroy');
+});
+
+
+
+require __DIR__.'/auth.php';
